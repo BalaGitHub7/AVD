822a578b27708029f9ca6ace097ff5df7622f5e0
BalaGitHub7 <bala7.rbk@gmail.com>
2024-08-16 10:31:19 +0200

    Add files via upload

---
+++ ImageManagement/Scripts/Install-AppxApplication.ps1
@@ -0,0 +1,89 @@
+[CmdletBinding()]
+param
+(
+    [parameter()]
+    [string]$WorkingPath = "C:\AppDeployment",
+
+    [parameter(Mandatory, Position = 1)]
+    [string]$AppInstallerLocation
+)
+
+Begin {
+    $date = get-date -format [yyyy-MM-dd_mm:ss]-
+    $date = $date.ToString()
+    if (-not(Test-Path $WorkingPath)) {
+        New-Item -ItemType Directory -Path $WorkingPath
+    }   
+    try {
+        if ($AppInstallerLocation.Contains('raw.githubusercontent.com')) {
+            Write-Verbose "Raw GitHub content provided"
+            $repoPath = ($AppInstallerLocation -split $($AppInstallerLocation -split "/")[5])[-1] 
+        }
+        else {
+            Write-Verbose "Human readable GitHub content provided"
+            $repoPath = ($AppInstallerLocation -split $($AppInstallerLocation -split "/")[6])[-1]
+        }
+        $arguments = 'https://api.github.com/repos/{3}/{4}/contents' -f ($AppInstallerLocation -split "/") + [string]$repoPath
+        $request = Invoke-WebRequest -Uri $($arguments) -UseBasicParsing:$true
+        $content = $request.Content | ConvertFrom-Json
+        $files = $content | Where-Object { $_.type -eq "file" } | Select-Object download_url, name
+        try {
+            $files.download_url -match '.appinstaller$'
+            $appName = ($files | Where-Object { $_.name -match '.appinstaller$' }).Name.Split(".")[0] 
+        }
+        catch {
+            throw "Location has no appinstaller file"
+        }
+    }
+    catch {
+        Write-Error "Location not found!!"
+        break
+    }
+}
+Process {
+    try {
+        Write-Verbose "Downloading files"
+        $AppWorkingPath = (Join-Path -Path (Join-Path -Path $WorkingPath -ChildPath 'MSIX') -ChildPath $appName)
+        if (-not(Test-Path $AppWorkingPath)) {
+            $AppWorkingPath = (New-Item -ItemType Directory $AppWorkingPath).FullName
+        }
+        $logFile = Join-Path -Path $AppWorkingPath -ChildPath 'install.log'
+        $files | ForEach-Object {
+            $requestParams = @{
+                Uri             = $_.Download_url
+                OutFile         = Join-Path -Path $AppWorkingPath -ChildPath $_.Name
+                UseBasicParsing = $true
+                Headers         = @{"Cache-Control" = "no-cache" }
+            }
+            Invoke-WebRequest @requestParams
+            Write-Output "Downloaded file $($_.Name)" | Out-File $logFile -Append
+        }
+        $appInstallerFile = Get-ChildItem -Path $AppWorkingPath | Where-Object { $_.Name.Endswith('.appinstaller') } | Sort-Object | Select-Object -Last 1
+        [xml]$packageInfo = Get-Content $appInstallerFile.FullName 
+        if ($null -ne $packageInfo.AppInstaller.MainBundle){
+            $MainPackageInfo = $packageInfo.AppInstaller.MainBundle
+        }
+        else {
+            $MainPackageInfo = $packageInfo.AppInstaller.MainPackage
+        }
+        $appPackage = $MainPackageInfo.Uri
+    }
+    catch {
+        Throw "Download package failed, $_"
+    }
+    try {
+        Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
+        if (-not (Get-Module -Name Appx -ListAvailable)) {
+            Install-Module -Name AppX -Force
+            Write-Output "Installing Appx PowerShell Module" | Out-File $logFile -Append
+        }
+        Import-Module Appx
+        Write-Output "Installing application based on $appPackage" | Out-File $logFile -Append
+        
+        Add-AppxProvisionedPackage -Online -SkipLicense -Packagepath $appPackage
+    }
+    catch {
+        Throw "Install AppX module failed, $_"
+    }
+    Write-Output "Script finished for $appName" | Out-File $logFile -Append
+}
---
+++ ImageManagement/Scripts/deploy-initial-vm.ps1
@@ -0,0 +1,80 @@
+param
+(
+    [Parameter (Mandatory)]
+    [string] $resourceGroupName,
+
+    [Parameter (Mandatory)]
+    [string] $VirtualMachineName,
+
+    [Parameter (Mandatory)]
+    [string] $ImageSKU,
+
+    [Parameter (Mandatory)]
+    [string] $ImageOffer,
+
+    [Parameter (Mandatory)]
+    [string] $ImagePublisher
+)
+
+function Get-RandomCharacters($length, $characters) {
+    $random = 1..$length | ForEach-Object { Get-Random -Maximum $characters.length }
+    $private:ofs = ""
+    return [String]$characters[$random]
+}
+function New-RandomString($type) {
+    if ($type -eq 'string') {
+        $username = Get-RandomCharacters -length 8 -characters 'abcdefghiklmnoprstuvwxyz'
+        return $username
+    }
+    if ($type -eq 'password') {
+        $password = Get-RandomCharacters -length 6 -characters 'abcdefghiklmnoprstuvwxyz'
+        $password += Get-RandomCharacters -length 2 -characters 'ABCDEFGHKLMNOPRSTUVWXYZ'
+        $password += Get-RandomCharacters -length 2 -characters '1234567890'
+        $password += Get-RandomCharacters -length 2 -characters '!%&()=#+'
+        return $password
+    }
+}
+
+$adminUsername = New-RandomString -type string
+$adminPassword = New-RandomString -type password
+$vmLocalAdminSecurePassword = ConvertTo-SecureString $adminPassword -AsPlainText -Force
+$Credential = New-Object System.Management.Automation.PSCredential ($adminUsername, $vmLocalAdminSecurePassword);
+$location = "WestEurope"
+$vmSize = "Standard_B2ms"
+$computerName = 'initvm'
+$diskSizeGB = 128
+
+
+$subnetParameters = @{
+    Name          = "sbn-$($VirtualMachineName)" 
+    AddressPrefix = "10.0.0.0/24"
+}
+$subnet = New-AzVirtualNetworkSubnetConfig @subnetParameters
+$vnetParameters = @{
+    Name              = "vnet-$($VirtualMachineName)" 
+    ResourceGroupName = $resourceGroupName
+    Location          = $location
+    AddressPrefix     = "10.0.0.0/16" 
+    Subnet            = $subnet
+}
+$vnet = New-AzVirtualNetwork @vnetParameters
+$subnetId = (Get-AzVirtualNetworkSubnetConfig -VirtualNetwork $vnet -Name $subnet.Name).Id
+$nicParameters = @{
+    Name              = "nic-$($VirtualMachineName)"
+    ResourceGroupName = $resourceGroupName
+    Location          = $location
+    SubnetId          = $subnetId
+}
+$nic = New-AzNetworkInterface @nicParameters
+Write-Output "##vso[task.setvariable variable=Subnet;isOutput=true]$($subnet.id)"
+Write-Output "Subnet is $subnet"
+
+
+$VirtualMachine = New-AzVMConfig -VMName $VirtualMachineName -VMSize $VMSize
+$VirtualMachine = Set-AzVMOperatingSystem -VM $VirtualMachine -Windows -ComputerName $computerName -Credential $Credential -ProvisionVMAgent -EnableAutoUpdate
+$VirtualMachine = Add-AzVMNetworkInterface -VM $VirtualMachine -Id $nic.Id
+$VirtualMachine = Set-AzVMOSDisk -Windows -VM $VirtualMachine -CreateOption FromImage -DiskSizeInGB $diskSizeGB
+$VirtualMachine = Set-AzVMSourceImage -VM $VirtualMachine -PublisherName $imagePublisher -Offer $imageOffer -Skus $imageSku -Version latest
+
+New-AzVM -ResourceGroupName $resourceGroupName -Location $location -VM $VirtualMachine
+Write-Host "##vso[task.setvariable variable=VirtualMachineName;isOutput=true;issecret=false]$VirtualMachineName"
